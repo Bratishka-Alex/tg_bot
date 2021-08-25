032612b8c8de8a8b17a7aec0e516455986ee7a4a
NoName2201 <77582762+NoName2201@users.noreply.github.com>
2021-08-25 03:43:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -21,14 +21,6 @@ myobj = {
     "remind": "2",
     "remind_unit": "h"
 }
-url_keys = {"sport": "Спорт",
-            "education": "Образование",
-            "roflxdlmao": "Развлечение",
-            "public_govno": "Общественная деаятельность",
-            "Спорт": "sport",
-            "Образование": "education",
-            "Развлечение": "roflxdlmao",
-            "Общественная деаятельность": "public_govno"}
 
 x = (requests.post(url, json=myobj)).json()
 tconv = lambda x: time.strftime("%H:%M:%S %d.%m.%Y", time.localtime(x))
@@ -68,12 +60,22 @@ def menu():
     return markup
 
 
-def menu2():
+def menu_authorized():
     markup = InlineKeyboardMarkup()
     markup.row_width = 1  # Ширина поля кнопок
-    fuck_u = InlineKeyboardButton("Жалобы (в работе)", callback_data="appeals")
+    appeals = InlineKeyboardButton("Жалобы", callback_data="appeals")
     exit = InlineKeyboardButton("Выйти из аккаунта", callback_data="exit")
-    markup.add(fuck_u, exit)
+    markup.add(appeals, exit)
+    return markup
+
+
+def menu_appeals():
+    markup = InlineKeyboardMarkup()
+    markup.row_width = 1  # Ширина поля кнопок
+    create__appeal = InlineKeyboardButton("Пожаловаться", callback_data="create__appeal")
+    my__appeals = InlineKeyboardButton("Мои жалобы", callback_data="my__appeals")
+    back_to_menu_authorized = InlineKeyboardButton('Назад', callback_data='back_to_menu_authorized')
+    markup.add(create__appeal, my__appeals, back_to_menu_authorized)
     return markup
 
 
@@ -116,7 +118,7 @@ def logging_in2(message, logs, id):
             try:
                 if json.loads(r.text)['user']:
                     bot.edit_message_text('Введите пароль:', message.chat.id, id)  # editing = 0
-                    a = bot.send_message(message.chat.id, 'Вы вошли в свой аккаунт!✅', reply_markup=menu2())  # editing = 4
+                    a = bot.send_message(message.chat.id, 'Вы вошли в свой аккаунт!✅', reply_markup=menu_authorized())  # editing = 4
                     globalVar[str(message.chat.id)]['message_id'] = str(a.message_id)
             except Exception:
                 bot.edit_message_text('Введите пароль:', message.chat.id, id)  # editing = 0
@@ -135,17 +137,17 @@ def logging_in2(message, logs, id):
         # editing = 4
         globalVar[str(message.chat.id)]['message_id'] = str(a.message_id)
 
-    deleting(globalVar, message.chat.id)
+    deleting(message.chat.id)
 
 
-def deleting(to_delete, chat_id):
+def deleting(chat_id):
     if len(globalVar[str(chat_id)]['to_delete']) != 0:
         for message in globalVar[str(chat_id)]['to_delete']:
             bot.delete_message(chat_id, message)
     globalVar[str(chat_id)]['to_delete'] = list()
 
 
-def check2(id):
+def check(id):
     s = requests.Session()
     payload = {"chat_id": str(id)}
     send_to = 'telegram/user'
@@ -164,12 +166,24 @@ def exit(id):
     s.post(f'{url}/{send_to}', json=payload)
 
 
+def back_to_menu_appeals():
+    markup = InlineKeyboardMarkup()
+    back_to_menu_appeals = InlineKeyboardButton('Назад', callback_data='back_to_menu_appeals')
+    markup.add(back_to_menu_appeals)
+    return markup
+
+
 def back3():
     markup = InlineKeyboardMarkup()
     back_to_menu = InlineKeyboardButton('Назад', callback_data='back_to_menu')
     markup.add(back_to_menu)
     return markup
 
+def back2():
+    markup = InlineKeyboardMarkup()
+    back_to_menu_authorized = InlineKeyboardButton('Назад', callback_data='back_to_menu_authorized')
+    markup.add(back_to_menu_authorized)
+    return markup
 
 def back():
     markup = InlineKeyboardMarkup()
@@ -179,14 +193,52 @@ def back():
     return markup
 
 
-def look_for_the_newest(globalvar):
-    max1 = 0
-    for key in globalvar:
-        key = str(key)
-        if int(globalvar[key]['message_id']) > max1:
-            max1 = int(globalvar[key]['message_id'])
-    return max1
+def create_appeal(message, bot_message_id):
+    a = bot.edit_message_text('Опишите вашу проблему:', message.chat.id, bot_message_id)
+    s = requests.Session()
+    id = message.chat.id
+    payload = {"text": message.text,"chat_id": str(id)}
+    send_to = 'appeals/create'
+    s.post(f'{url}/{send_to}', json=payload)
+    globalVar[str(message.chat.id)]['to_delete'].append(bot_message_id)
+    globalVar[str(message.chat.id)]['to_delete'].append(message.message_id)
+    a = bot.send_message(id, 'Ваша жалоба принята💀', reply_markup=back_to_menu_appeals())
+    globalVar[str(cmcd)]['message_id'] = str(a.message_id)
 
+
+def my_appeals(bot_message_id, id):
+    s = requests.Session()
+    payload = {"chat_id": str(id)}
+    send_to = 'appeals/my'
+    r = s.get(f'{url}/{send_to}', json=payload)
+    try:
+        a = json.loads(r.text)['appeals']
+        if json.loads(r.text)['appeals'] and len(a)!=0:
+            for appeal_id in range(len(a)):
+                t = a[appeal_id]
+                status = str(t['status'])
+                date = str(t['date'])[:10].split('-')
+                text = str(t['text'])
+                if status == 'waiting':
+                    status = 'Ожидание'
+                elif status == 'in_work':
+                    status = 'В работе'
+                elif status == 'done':
+                    status = "Выполнено"
+                elif status == 'rejected':
+                    status = 'Отклонено'
+                b = bot.send_message(id, f'Дата: *{date[2]}.{date[1]}.{date[0]}*\nСтатус: *{status}*\n*{text}*',
+                                     parse_mode="Markdown")
+                globalVar[str(id)]['to_delete'].append(b.message_id)
+        else:
+            b = bot.send_message(id, 'Вы не отправляли ни одной жалобы')
+            globalVar[str(id)]['to_delete'].append(b.message_id)
+    except Exception:
+        b = bot.send_message(id, 'Вы не отправляли ни одной жалобы')
+        globalVar[str(id)]['to_delete'].append(b.message_id)
+    globalVar[str(id)]['to_delete'].append(bot_message_id)
+    a = bot.send_message(id, 'Выберите действие', reply_markup=back_to_menu_appeals())
+    globalVar[str(cmcd)]['message_id'] = str(a.message_id)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -198,43 +250,58 @@ def send_welcome(message):
 
         globalVar[str(message.chat.id)] = {}
         globalVar[str(message.chat.id)]['to_delete'] = list()
+        globalVar[str(message.chat.id)]['topic'] = None
+        globalVar[str(message.chat.id)]['error_messages'] = list()
         globalVar[str(message.chat.id)]['message_id'] = str(message.message_id)
 
-    deleting(globalVar, message.chat.id)
+    deleting(message.chat.id)
+    try:
+        bot.delete_message(message.chat.id, int(globalVar[str(message.chat.id)]['error_messages']))
+    except Exception:
+        None
 
-    if check2(message.chat.id):
-        if int(globalVar[str(message.chat.id)]['message_id']) != message.message_id:
-            bot.delete_message(message.chat.id, int(globalVar[str(message.chat.id)]['message_id']))
-            globalVar[str(message.chat.id)]['message_id'] = str(message.message_id + 1)
+    if check(message.chat.id):
         s = requests.Session()
         payload = {"chat_id": message.chat.id}
         send_to = 'telegram/user'
         r = s.get(f'{url}/{send_to}', json=payload)
+        firstname = json.loads(r.text)['user']['fullname'].split()[1].capitalize()
         bot.delete_message(message.chat.id, message.message_id)
-        a = bot.send_message(message.chat.id, f"С возвращением, *{json.loads(r.text)['user']['firstname'].capitalize()}"
-                                          f"*!\nВыберите действие:", reply_markup=menu2(), parse_mode="Markdown")
-    else:
+        a = bot.send_message(message.chat.id, f"С возвращением, *{firstname}*!\nВыберите действие:",
+                             reply_markup=menu_authorized(), parse_mode="Markdown")
         if int(globalVar[str(message.chat.id)]['message_id']) != message.message_id:
             bot.delete_message(message.chat.id, int(globalVar[str(message.chat.id)]['message_id']))
-            globalVar[str(message.chat.id)]['message_id'] = str(look_for_the_newest(globalVar) + 1)
-
+            globalVar[str(message.chat.id)]['message_id'] = str(a.message_id + 1)
+    else:
         bot.delete_message(message.chat.id, message.message_id)
         a = bot.send_message(message.chat.id, "Здравствуйте!\nВыберите действие:", reply_markup=menu())
+        if int(globalVar[str(message.chat.id)]['message_id']) != message.message_id:
+            bot.delete_message(message.chat.id, int(globalVar[str(message.chat.id)]['message_id']))
+            globalVar[str(message.chat.id)]['message_id'] = str(a.message_id + 1)
 
-
+    if globalVar[str(message.chat.id)]['topic'] != None:
+        bot.delete_message(message.chat.id, int(globalVar[str(message.chat.id)]['topic']))
+        globalVar[str(message.chat.id)]['topic'] = None
     globalVar[str(message.chat.id)]['message_id'] = str(a.message_id)
 
 
 @bot.message_handler(content_types=['text', 'photo', 'video', 'document', 'audio', 'voice', 'sticker', 'contact'])
 def error(message):
+    if str(message.chat.id) not in globalVar:
+        globalVar[str(message.chat.id)] = {}
+        globalVar[str(message.chat.id)]['to_delete'] = list()
+        globalVar[str(message.chat.id)]['topic'] = None
+        globalVar[str(message.chat.id)]['error_messages'] = None
+        globalVar[str(message.chat.id)]['message_id'] = str(message.message_id)
+
     try:
-        deleting(globalVar, message.chat.id)
-        bot.delete_message(message.chat.id, message.message_id)
+        bot.delete_message(message.chat.id, int(globalVar[str(message.chat.id)]['error_messages']))
     except Exception:
-        pass
+        None
+    bot.delete_message(message.chat.id, message.message_id)
     a = bot.send_message(message.chat.id, 'Воспользуйтесь предложенными кнопками. '
                                       'Если кнопки исчезли, введите команду /start')
-    globalVar[str(message.chat.id)]['to_delete'].append(a.message_id)
+    globalVar[str(message.chat.id)]['error_messages'] = a.message_id
 
 
 @bot.callback_query_handler(func=lambda call: True)
@@ -244,30 +311,64 @@ def callback_query(call):
         cmcd = call.message.chat.id
         cmmi = call.message.message_id
         print(cmcd, call.data)
+        try:
+            bot.delete_message(cmcd, int(globalVar[str(cmcd)]['error_messages']))
+        except Exception:
+            None
 
         if call.data == "log_in":
-            b = bot.edit_message_text("Введите адрес электронной почты для авторизации:",
+            a = bot.edit_message_text("Введите адрес электронной почты для авторизации:",
                                   cmcd, cmmi, reply_markup=back3())
-            deleting(globalVar, cmcd)
-            bot.register_next_step_handler(b, logging_in, cmmi)
+            deleting(cmcd)
+            bot.register_next_step_handler(a, logging_in, cmmi)
+
         elif call.data == 'registration':
             bot.delete_message(cmcd, cmmi)
             a = bot.send_message(cmcd, f'Регистрация проходит на сайте:\n {url}', reply_markup=back3())
             globalVar[str(cmcd)]['message_id'] = str(a.message_id)
 
         elif call.data == 'appeals':
-            bot.edit_message_text('Жалобы', cmcd, cmmi, reply_markup=menu2())
+            bot.delete_message(cmcd,cmmi)
+            a = bot.send_message(cmcd, '*Жалобы*', parse_mode="Markdown")
+            b = bot.send_message(cmcd, 'Выберите действие:', reply_markup=menu_appeals())
+            deleting(cmcd)
+            globalVar[str(cmcd)]['topic'] = str(a.message_id)
+            globalVar[str(cmcd)]['message_id'] = str(b.message_id)
+            print(globalVar)
+
+        elif call.data == 'create__appeal':
+            a = bot.edit_message_text('Опишите вашу проблему:', cmcd, cmmi, reply_markup=back_to_menu_appeals())
+            bot.register_next_step_handler(a, create_appeal, cmmi)
+
+        elif call.data == 'my__appeals':
+            a = bot.edit_message_text('Ваши жалобы:', cmcd, cmmi)
+            my_appeals(cmmi,cmcd)
 
         elif call.data == 'exit':
             exit(call.message.chat.id)
+            deleting(cmcd)
             bot.edit_message_text('Вы вышли из аккаунта❗', cmcd, cmmi, reply_markup=menu())
 
         elif call.data == 'back_to_menu':
             bot.clear_step_handler_by_chat_id(cmcd)  # Может это вызывает ошибку при входе с двух устройств одновременно
-            deleting(globalVar, cmcd)
+            deleting(cmcd)
             bot.edit_message_text('Выберите действие:', cmcd, cmmi, reply_markup=menu())
 
+        elif call.data == 'back_to_menu_authorized':
+            bot.delete_message(cmcd, int(globalVar[str(cmcd)]['topic']))
+            globalVar[str(cmcd)]['topic'] = None
+            bot.clear_step_handler_by_chat_id(cmcd)
+            deleting(cmcd)
+            bot.edit_message_text('Выберите действие:', cmcd, cmmi, reply_markup=menu_authorized())
+
+        elif call.data == 'back_to_menu_appeals':
+            bot.clear_step_handler_by_chat_id(cmcd)
+            deleting(cmcd)
+            bot.edit_message_text('Выберите действие:', cmcd, cmmi, reply_markup=menu_appeals())
+
+
         bot.answer_callback_query(call.id)
+
     except Exception as e:
         print(e)
         pass
